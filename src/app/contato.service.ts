14adce89e5a98941a157138afb30c30fb9d94b9b
anabeatrizgoncalves <aanabbeatrizzin@gmail.com>
2023-05-19 11:59:39 -0300

    commit inicial

---
+++ src/app/contato.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Contato } from './contato/contato';
+import { Observable } from 'rxjs';
+import { environment } from './environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContatoService {
+
+  url: string = environment.apiBaseUrl
+
+  constructor(
+    private http: HttpClient
+
+  ) { }
+
+  save(contato: Contato) : Observable<Contato>{
+    return this.http.post<Contato>(this.url, contato);
+  }
+
+    list() : Observable<Contato[]>{
+      return this.http.get<any>(this.url)
+
+    }
+
+    favorite(contato: Contato) : Observable<any>{
+       return this.http.patch(`${this.url}/${contato.id}/favorito`, null);
+    }
+
+    upload(contato: Contato, formData: FormData) : Observable<any>{
+       return this.http.put(`${this.url}/ ${contato.id}/foto`, formData, {responseType: 'blob'});
+    }
+}
---
+++ src/app/contato/contato.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { Contato } from './contato';
+import { ContatoService } from '../contato.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { MatDialog } from '@angular/material/dialog';
+import {ContatoDetalheComponent} from '../contato-detalhe/contato-detalhe.component'
+
+
+@Component({
+  selector: 'app-contato',
+  templateUrl: './contato.component.html',
+  styleUrls: ['./contato.component.css']
+})
+export class ContatoComponent implements OnInit {
+
+  formulario!: FormGroup;
+  contatos: Contato[] = [];
+  colunas = ['foto','id', 'nome', 'email', 'favorito']
+
+  constructor(
+    private service: ContatoService,
+    private fb: FormBuilder,
+    private dialog: MatDialog
+  ) { }
+  
+  ngOnInit(): void {
+    this.montarFormulario();
+    this.listarContatos();
+  }
+
+     montarFormulario(){
+      this.formulario = this.fb.group({
+        nome: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        
+      })
+     }
+
+  listarContatos() {
+    this.service.list().subscribe(response => {
+      this.contatos = response;
+    })
+  }
+
+    favoritar(contato : Contato){
+        this.service.favorite(contato).subscribe( responde =>{
+          contato.favorito = !contato.favorito;
+        })
+    }
+
+  submit() {
+    const formValues = this.formulario.value;
+    const contato: Contato = new Contato(formValues.nome, formValues.email);
+    this.service.save(contato).subscribe(resposta => {
+      let lista: Contato[]= [... this.contatos, resposta]
+      this.contatos = lista;
+    })
+
+    
+  }
+
+    uploadFoto(event: any, contato: any){
+           const files = event.target.files;
+          if(files){
+            const foto = files[0];
+            const formData: FormData = new FormData();
+            formData.append("foto", foto);
+              this.service
+                .upload(contato, formData)
+                .subscribe(response => this.listarContatos());
+          }
+   }
+  
+     visualizarContato(contato: Contato){
+         this.dialog.open(ContatoDetalheComponent, {
+            width: '400px',
+            height: '450px',
+            data: contato
+
+         })
+     }
+
+}
